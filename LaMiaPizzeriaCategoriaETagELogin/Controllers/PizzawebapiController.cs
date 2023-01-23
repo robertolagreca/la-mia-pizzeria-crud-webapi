98a19534275d4044fb921516f6c11e6733fed041
robertolagreca <robertolagreca92@gmail.com>
2023-01-23 15:35:55 +0100

    Metodo Get Web Api

---
+++ LaMiaPizzeriaCategoriaETagELogin/Controllers/PizzawebapiController.cs
@@ -0,0 +1,26 @@
+﻿using LaMiaPizzeriaCategoriaETagELogin.Database;
+using LaMiaPizzeriaCategoriaETagELogin.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LaMiaPizzeriaCategoriaETagELogin.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PizzawebapiController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            using (PizzaContext db = new PizzaContext())
+            {
+                List<Pizza> pizzasWebApi = db.Pizzas.Include(pizza => pizza.Tags).ToList<Pizza>();
+
+                return Ok(pizzasWebApi);
+            }
+        }
+
+    }
+}
