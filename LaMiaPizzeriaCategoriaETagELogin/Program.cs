f673a7d7fe566cca2b5f48550d44e7070ac94c8b
robertolagreca <robertolagreca92@gmail.com>
2023-01-23 16:12:05 +0100

    Modifica Index di Home

--- LaMiaPizzeriaCategoriaETagELogin/Program.cs
+++ LaMiaPizzeriaCategoriaETagELogin/Program.cs
@@ -39,7 +39,8 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Pizza}/{action=Index}/{id?}");
+    //pattern: "{controller=Pizza}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.MapRazorPages();
 
